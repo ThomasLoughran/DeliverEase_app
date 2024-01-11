57d58e5c9ec8822f9904e642595aeb229053bd50
Thomas Loughran <tloughran3@gmail.com>
2024-01-11 10:16:44 +0000

    empty project

---
+++ server/webapp/src/test/java/com/deliverease_group/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliverease_group.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
