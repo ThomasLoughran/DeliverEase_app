a0c6a50aa5eca071653e2c57d25368a2237a5967
Billie Redwood <billieredwood@gmail.com>
2024-01-12 12:13:09 +0000

    added get all drivers by distCentId with error handling.

--- server/webapp/src/main/java/com/deliverease_group/webapp/controllers/DriverController.java
+++ server/webapp/src/main/java/com/deliverease_group/webapp/controllers/DriverController.java
@@ -1,12 +1,36 @@
 package com.deliverease_group.webapp.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.deliverease_group.webapp.models.Driver;
+import com.deliverease_group.webapp.models.Employee;
+import com.deliverease_group.webapp.models.Role;
+import com.deliverease_group.webapp.services.DriverService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/drivers")
+
 public class DriverController {
 
     // TODO  - GET routes by driverId, PATCH update availability, PATCH order is successful/unsuccessful, * PATCH change distribution centre (EXTENSION) *
+
+    @Autowired
+    DriverService driverService;
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllDriversByDistCentId(@RequestParam Long distCentId){
+        try {
+            return new ResponseEntity<>(driverService.findAllDriverIdsByDistCentId(distCentId, Role.DRIVER), HttpStatus.OK);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
 
 }
--- server/webapp/src/main/java/com/deliverease_group/webapp/services/DriverService.java
+++ server/webapp/src/main/java/com/deliverease_group/webapp/services/DriverService.java
@@ -1,7 +1,22 @@
 package com.deliverease_group.webapp.services;
 
+import com.deliverease_group.webapp.models.Driver;
+import com.deliverease_group.webapp.models.Employee;
+import com.deliverease_group.webapp.models.Role;
+import com.deliverease_group.webapp.repositories.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
 public class DriverService {
 
-    
+    @Autowired
+    EmployeeRepository employeeRepository;
+    public List<Employee> findAllDriverIdsByDistCentId(Long distCentId, Role role) {
+
+        return employeeRepository.findAllByDistributionCentreIdAndRole(distCentId, role);
+    }
+
 
 }
