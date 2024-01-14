db63217a1f204e24b92ac60d2aa28afbd57c6ff6
Albert Hughes <albieh@me.com>
2024-01-14 13:47:44 +0000

    added all driver information each li generated in driversList

--- client/src/components/ManagerDashboardComponents/DriversList.js
+++ client/src/components/ManagerDashboardComponents/DriversList.js
@@ -2,12 +2,33 @@ const DriversList = ({ drivers }) => {
 
     console.log(drivers);
 
+    const driversListComponents = drivers.map((driver) => {
+        console.log(driver);
+        return (
+
+            <li className="driver-card">
+                <p>Id: {driver.id}</p>
+                <p>Name: {driver.name}</p>
+                <p>Van Capacity: {driver.vanCapacity}</p>
+                <p>Van Max Weight: {driver.vanMaxWeight}</p>
+                <p>Van name: {driver.vanName}</p>
+                <p>Available Dates: {driver.availableDates}</p>
+            </li>
+        )
+    })
+
 
 
     return ( 
-        <p>Hello from DriversList</p>
+        <>
+            <p>Hello from DriversList</p>
+            <ul>
+            {drivers ? driversListComponents : <p>Loading</p>}
+            </ul>
+        </>
+        
 
-     );
+    );
 }
  
 export default DriversList;
