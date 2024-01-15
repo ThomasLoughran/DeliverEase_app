8c0cbe4136fb5c2897041603ad214977a4567dfa
Billie Redwood <billieredwood@gmail.com>
2024-01-15 10:44:33 +0000

    added current date information to display in routes calendar.

--- client/src/components/ManagerDashboardComponents/RouteCalendar.js
+++ client/src/components/ManagerDashboardComponents/RouteCalendar.js
@@ -1,8 +1,13 @@
-const RouteCalendar = () => {
+const RouteCalendar = ({date}) => {
+
+    console.log(date);
+
+
     return ( 
-
+        <>
         <h1>Hello from route calendar</h1>
-
+        <p>Day Selected: {date.toLocaleDateString()}</p>
+        </>
      );
 }
  
--- client/src/containers/ManagerRoutesContainer.js
+++ client/src/containers/ManagerRoutesContainer.js
@@ -1,11 +1,18 @@
+import { useEffect, useState } from "react";
 import RouteCalendar from "../components/ManagerDashboardComponents/RouteCalendar";
 
 const ManagerRoutesContainer = () => {
 
+    const [date, setDate] = useState (new Date());
+    
+    
+    useEffect(() => {
+        // console.log(date);
+    }, [date])
 
     return ( 
 
-            <RouteCalendar/>
+            <RouteCalendar date={date} />
 
      );
 }
