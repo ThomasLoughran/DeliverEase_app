aca91642041c52e0aefd88e724f0c7cdd61f1ebf
Albert Hughes <albieh@me.com>
2024-01-15 20:12:47 +0000

    modal is behaves like a real modal now

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,13 +1,16 @@
 import AddDriverForm from "./ManagerDashboardComponents/AddDriverForm";
+import '../styles/Modal.css'
 
 const Modal = ({closeModal}) => {
 
     return ( 
-        <div className="modal-background">
-            <div className="modal-container">
-                <button onClick={() => closeModal(false)}>X</button>
-                <div className="driver-form">
-                    <AddDriverForm/>
+        <div className="modal">
+            <div className="modal-background">
+                <div className="modal-container">
+                    <button onClick={() => closeModal(false)} className="close-modal-button">X</button>
+                    <div className="driver-form">
+                        <AddDriverForm/>
+                    </div>
                 </div>
             </div>
         </div>
