87665a4d75ace57b35c9af0dffd32af4ce9a2be6
Piraven Nandakumaran <Xothspath@gmail.com>
2024-01-18 19:40:33 +0000

    can longer drag waypoints across map

--- client/src/components/map/Routing.jsx
+++ client/src/components/map/Routing.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import L from "leaflet";
 import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
 import "leaflet-routing-machine";
@@ -8,39 +8,56 @@ L.Marker.prototype.options.icon = L.icon({
 });
 export default function Routing({currentOrder, previousOrder,orderWayPoints}) {
 
+  const [count,setCount] = useState(0);
+
   const map = useMap();
 
   useEffect(() => {
     console.log("previous", previousOrder.latitude, previousOrder.longitude)
     console.log("current", currentOrder.latitude, currentOrder.longitude)
-    
-    //console.log(orderWayPoints,"All waypoints in map");
-    if (previousOrder) {
+    setCount(count+1)
+    if (count<2){
 
-      if (!map) return;
 
     
     const coords = orderWayPoints.map((waypoint)=>{
         return L.latLng(waypoint[0],waypoint[1])
       }) 
 
-    console.log(coords);
+    //console.log(coords);
+    
+    
+    // const routingControl = L.Routing.control({
+    //   waypoints: coords,
+    //   lineOptions : {
+    //     draggableWaypoints:false
+    //   }
+    //   //routeWhileDragging: true
+    // }).addTo(map);
 
     const routingControl = L.Routing.control({
-      waypoints: coords
-      // routeWhileDragging: true,
-    }).addTo(map);
+      waypoints: coords,
+      fitSelectedRoutes: true,
+      draggableWaypoints: false,
+      routeWhileDragging: false,
+      //createMarker: function() { return null; },
+      lineOptions: {
+          addWaypoints: false,
+          styles: [{ color: '#242c81', weight: 2 }]
+      }
+     }).addTo(map);
+  
 
-
-    return () =>{
-      console.log("this is the map property", routingControl)
-      map.removeControl(routingControl);
-    }
-    }
-
+    // return () =>{
+    //   //console.log("this is the map property", routingControl)
+    //   //if (!map.routingControl){
+    //   //map.removeControl(routingControl);
+    //   //}
+    // }
+   }
     
 
-  }, [currentOrder, previousOrder,orderWayPoints]);
+  }, [orderWayPoints]);
 
   
 
